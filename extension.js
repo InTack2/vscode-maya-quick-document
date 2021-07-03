e0506d33b3511d40e9034fba17d807f5c6852190
Tack2 <takumi236@gmail.com>
2021-07-04 02:49:49 +0900

    delete japanese comment.

--- extension.js
+++ extension.js
@@ -11,27 +11,11 @@ function open_url_to_browser(url) {
     open(url);
 }
 
-
-// このメソッドは、拡張機能が起動したときに呼び出されます。
-// コマンドが初めて実行されたときに、拡張機能が有効になります。
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-    // コンソールを使用して、診断情報 (console.log) およびエラー (console.error) を出力します。
-    // この行のコードは、拡張機能が有効になったときに一度だけ実行されます
     console.log('Congratulations, your extension "maya-quick-document" is now active!');
-
-    // コマンドは package.json ファイルで定義されています。
-    // 次に、コマンドの実装を registerCommand で提供します。
-    // commandIdパラメータはpackage.jsonのcommandフィールドと一致しなければなりません。
-    // let disposable = vscode.commands.registerCommand('maya-quick-document.helloWorld', function () {
-    // 	// ここに配置したコードは、コマンドが実行されるたびに実行される
-
-    // 	// ユーザーにメッセージボックスを表示する
-    // 	vscode.window.showInformationMessage('Hello World from maya-quick-document!');
-    // });
 
     let disposable_2 = vscode.commands.registerCommand("maya-quick-document.show-document", function () {
 
@@ -57,17 +41,14 @@ function activate(context) {
                 word = document.getText(selection);
             } else {
                 const now_position = editor.selection.active;
-                const position = new vscode.Position(now_position.line, 0);
 
                 console.log(now_position);
-                console.log(position);
 
-                const target_range = new vscode.Range(position, now_position);
                 let hit_range = undefined;
-
                 try {
+
                     hit_range = document.getWordRangeAtPosition(now_position, /[a-zA-Z0-9_]+/);
-                    // const hit_range = document.getWordRangeAtPosition(target_range, /[a-zA-Z0-9_]+/);
+
                 } catch (error) {
                     console.log(error);
                 }
